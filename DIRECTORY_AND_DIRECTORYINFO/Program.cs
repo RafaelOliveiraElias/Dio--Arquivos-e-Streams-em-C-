25d9332fe54704e9d990e95b4f7296ba0ab8a5a2
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-22 14:39:33 -0300

    feat: directories

---
+++ DIRECTORY_AND_DIRECTORYINFO/Program.cs
@@ -0,0 +1,20 @@
+﻿var path = Path.Combine(Environment.CurrentDirectory, "globo");
+
+var dirGlobo = Directory.CreateDirectory(path);
+
+var americaNorte = dirGlobo.CreateSubdirectory("America do Norte");
+var americaCentral = dirGlobo.CreateSubdirectory("America Central");
+var americaSul = dirGlobo.CreateSubdirectory("America do Sul");
+
+
+americaNorte.CreateSubdirectory("USA");
+americaNorte.CreateSubdirectory("Mexico");
+americaNorte.CreateSubdirectory("Canada");
+
+
+americaCentral.CreateSubdirectory("Costa Rica");
+americaCentral.CreateSubdirectory("Panama");
+
+americaSul.CreateSubdirectory("Brasil");
+americaSul.CreateSubdirectory("Argentina");
+americaSul.CreateSubdirectory("Paraguai");
